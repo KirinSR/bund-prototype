f2bc25e82729aa26ffae02cc4b03c9aff1e723ea
SambitRanjan <18ec012@charusat.edu.in>
2024-07-08 06:18:29 +0530

    Initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,395 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("Horizontal Movement Settings:")]
+    [SerializeField] private float walkSpeed = 10f;
+    [SerializeField] private float acceleration = 20f;
+    [SerializeField] private float deceleration = 25f;
+    [Space(5)]
+
+
+    [Header("Vertical Movement Settings:")]
+    [SerializeField] private float jumpForce = 45f;
+    private int jumpBufferCounter = 0;
+    [SerializeField] private int jumpBufferFrames;
+    private float coyoteTimeCounter = 0;
+    [SerializeField] private float coyoteTime;
+    private int airJumpCounter = 0;
+    [SerializeField] private int maxAirJumps;
+    [Space(5)]
+
+    [Header("Wall Running Settings:")]    
+    [SerializeField] private float wallRunSpeed = 10f;
+    [SerializeField] private float wallRunDuration = 1f;
+    private float wallRunTimer;
+    private bool isWallRunning;
+    [Space(5)]
+
+    [Header("Wall Sliding/Jumping Settings:")]
+    [SerializeField] private float wallJumpingTime = 0.2f;
+    private bool isWallSliding;
+    private bool isWallJumping;
+    private float wallJumpingDirection;
+    private float wallJumpingCounter;
+    [SerializeField] private float wallJumpingDuration = 0.4f;
+    [SerializeField] private Vector2 wallJumpingPower = new Vector2(8f, 16f);
+    [SerializeField] private float wallSlidingSpeed = 2f;
+    [SerializeField] private Transform wallCheck;
+    [SerializeField] private LayerMask whatIsWall;
+    [Space(5)]
+
+
+    [Header("Ground Check Settings:")]
+    [SerializeField] private Transform groundCheckPoint;
+    [SerializeField] private float groundCheckY = 0.2f;
+    [SerializeField] private float groundCheckX = 0.5f;
+    [SerializeField] private LayerMask whatIsGround;
+    [Space(5)]
+
+
+    [Header("Dash Settings:")]
+    [SerializeField] private float dashSpeed;
+    [SerializeField] private float dashTime;
+    [SerializeField] private float dashCooldown;
+    [SerializeField] GameObject dashEffect;
+    [Space(5)]
+
+    [Header("Attack Settings:")]
+    [SerializeField] float damage;
+    [SerializeField] LayerMask attackableLayer;
+    [SerializeField] GameObject slashEffect;
+    private bool attack;
+    float timeBetweenAttack, timeSinceAttack;
+    [SerializeField] Transform SideAttackTransform, UpAttackTransform, DownAttackTransform;
+    [SerializeField] Vector2 SideAttackArea, UpAttackArea, DownAttackArea; 
+    [Space(5)]
+
+    PlayerStateList pState;
+    private Rigidbody2D rb;
+    private float xAxis, yAxis;
+    private float gravity;
+    Animator anim;
+    private bool canDash = true;
+    private bool dashed;
+
+
+    //creates a singleton of the PlayerController
+    public static PlayerController Instance;
+
+    private void Awake()
+    {
+        if(Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Instance = this;
+        }
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pState = GetComponent<PlayerStateList>();
+
+        rb = GetComponent<Rigidbody2D>();
+
+        anim = GetComponent<Animator>();
+
+        gravity = rb.gravityScale; 
+    }
+
+    void OnDrawGizmos() 
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(SideAttackTransform.position, SideAttackArea);
+        Gizmos.DrawWireCube(UpAttackTransform.position, UpAttackArea);
+        Gizmos.DrawWireCube(DownAttackTransform.position, DownAttackArea);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetInputs();
+        UpdateJumpVariables();
+        if (pState.dashing) return;
+        if (!isWallJumping) {Flip();}
+        Move();
+        Jump();
+        StartDash();
+        WallSlide();
+        WallRun();
+        WallJump();
+        Attack();
+    }
+
+    void GetInputs()
+    {
+        xAxis = Input.GetAxisRaw("Horizontal");
+        yAxis = Input.GetAxisRaw("Vertical");
+        attack = Input.GetMouseButtonDown(0);
+    }
+
+    void Flip()
+    {
+        if (xAxis < 0)
+        {
+            transform.localScale = new Vector2(-1, transform.localScale.y);
+        }
+        else if (xAxis > 0)
+        {
+            transform.localScale = new Vector2(1, transform.localScale.y);
+        }
+    }
+
+    private void Move()
+    {
+        if (!isWallJumping) {
+            float targetSpeed = xAxis * walkSpeed;
+            float speedDifference = targetSpeed - rb.velocity.x;
+            float accelerationRate = (Mathf.Abs(targetSpeed) > 0.01f) ? acceleration : deceleration;
+            float movement = Mathf.Pow(Mathf.Abs(speedDifference) * accelerationRate, 0.9f) * Mathf.Sign(speedDifference);
+
+            rb.velocity = new Vector2(rb.velocity.x + movement * Time.deltaTime, rb.velocity.y);
+            bool isMoving = Mathf.Abs(rb.velocity.x) > 0.01f;
+            anim.SetBool("Walking", isMoving && Grounded());
+        }
+    }
+
+    void StartDash()
+    {
+        if(Input.GetButtonDown("Dash") && canDash && !dashed)
+        {
+            StartCoroutine(Dash());
+            dashed = true;
+        }
+
+        if (Grounded())
+        {
+            dashed = false;
+        }
+    }
+
+    IEnumerator Dash()
+    {
+        canDash = false;
+        pState.dashing = true;
+        anim.SetTrigger("Dashing");
+        rb.gravityScale = 0;
+        rb.velocity = new Vector2(transform.localScale.x * dashSpeed, 0);
+        if (Grounded()) Instantiate(dashEffect, transform);
+        yield return new WaitForSeconds(dashTime);
+        rb.gravityScale = gravity;
+        pState.dashing = false;
+        yield return new WaitForSeconds(dashCooldown);
+        canDash = true;
+    }
+
+    public bool Grounded()
+    {
+        if (Physics2D.Raycast(groundCheckPoint.position, Vector2.down, groundCheckY, whatIsGround) 
+            || Physics2D.Raycast(groundCheckPoint.position + new Vector3(groundCheckX, 0, 0), Vector2.down, groundCheckY, whatIsGround) 
+            || Physics2D.Raycast(groundCheckPoint.position + new Vector3(-groundCheckX, 0, 0), Vector2.down, groundCheckY, whatIsGround))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public bool Walled()
+    {
+        return Physics2D.OverlapCircle(wallCheck.position, 0.2f, whatIsWall);
+    }
+
+    void WallSlide()
+    {
+        if (Walled() && !Grounded() && xAxis != 0f)
+        {
+            isWallSliding = true;
+            rb.velocity = new Vector2(rb.velocity.x, Mathf.Clamp(rb.velocity.y, -wallSlidingSpeed, float.MaxValue));
+        }
+        else
+        {
+            isWallSliding = false;
+        }
+    }
+
+    void WallRun()
+    {
+        if (Walled() && Input.GetButtonDown("Run"))
+        {
+            isWallRunning = true;
+            wallRunTimer = wallRunDuration;
+        }
+
+        if (isWallRunning)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, wallRunSpeed);
+            wallRunTimer -= Time.deltaTime;
+
+            if (wallRunTimer <= 0 || !Walled())
+            {
+                isWallRunning = false;
+            }
+        }
+    }
+
+
+    void  WallJump()
+    {
+        if (isWallSliding)
+        {
+            isWallJumping = false;
+            wallJumpingDirection = -transform.localScale.x;
+            wallJumpingCounter = wallJumpingTime;
+
+            CancelInvoke(nameof(StopWallJumping));
+
+        }
+        else 
+        {
+            wallJumpingCounter -= Time.deltaTime;
+        }
+
+        if (Input.GetButtonDown("Jump") && wallJumpingCounter > 0f)
+        {
+            isWallJumping = true;
+
+            if (xAxis == 0)
+            {
+                rb.velocity = new Vector2(wallJumpingDirection * wallJumpingPower.x, 0);                
+            }
+            else{
+                rb.velocity = new Vector2(wallJumpingDirection * wallJumpingPower.x, wallJumpingPower.y);
+            }
+
+            wallJumpingCounter = 0f;
+            
+            if (transform.localScale.x != wallJumpingDirection)
+            {   
+                Flip();
+            }
+
+            Invoke(nameof(StopWallJumping), wallJumpingDuration);
+        
+        }
+    }
+
+
+    private void StopWallJumping()
+    {
+        isWallJumping = false;;
+    }
+
+
+    void Jump()
+    {
+        if(Input.GetButtonUp("Jump") && rb.velocity.y > 0)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, 0);
+
+            pState.jumping = false;
+        }
+
+        if (!pState.jumping)
+        {
+            if (jumpBufferCounter > 0 && coyoteTimeCounter > 0)
+            {
+                rb.velocity = new Vector3(rb.velocity.x, jumpForce);
+
+                pState.jumping = true;
+            }
+            else if(!Grounded() && airJumpCounter < maxAirJumps && Input.GetButtonDown("Jump"))
+            {
+                pState.jumping = true;
+
+                airJumpCounter++;
+
+                rb.velocity = new Vector3(rb.velocity.x, jumpForce);
+            }
+        }
+
+        anim.SetBool("Jumping", !Grounded());
+    }
+
+    void UpdateJumpVariables()
+    {
+        if (Grounded())
+        {
+            pState.jumping = false;
+            coyoteTimeCounter = coyoteTime;
+            airJumpCounter = 0;
+        }
+        else
+        {
+            coyoteTimeCounter -= Time.deltaTime;
+        }
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jumpBufferCounter = jumpBufferFrames;
+        }
+        else
+        {
+            jumpBufferCounter--;
+        }
+    }
+
+    void Attack()
+    {
+        timeSinceAttack += Time.deltaTime;
+        if (attack && timeSinceAttack >= timeBetweenAttack)
+        {
+            timeSinceAttack = 0;
+            anim.SetTrigger("Attacking");
+
+            if (yAxis == 0 || yAxis < 0 && Grounded()){
+                Hit(SideAttackTransform, SideAttackArea);
+                Instantiate(slashEffect, SideAttackTransform);
+            }
+            else if (yAxis > 0)
+            {
+                Hit(DownAttackTransform, DownAttackArea);
+                SlasheffectAngle(slashEffect, 90, UpAttackTransform);
+
+            }
+            else if (yAxis < 0 && !Grounded())
+            {
+                Hit(UpAttackTransform, UpAttackArea);
+                SlasheffectAngle(slashEffect, -90, DownAttackTransform);
+            }
+        }
+    }
+
+    private void Hit(Transform _attackTransform, Vector2 _attackArea)
+    {
+        Collider2D[] objectsToHit = Physics2D.OverlapBoxAll(_attackTransform.position, _attackArea, 0, attackableLayer);
+        if (objectsToHit.Length > 0)
+        {
+            Debug.Log("Hit");
+        }
+
+        for (int i = 0; i < objectsToHit.Length; i++)
+        {
+            if (objectsToHit[i].GetComponent<Enemy>() != null)
+            {
+                objectsToHit[i].GetComponent<Enemy>().EnemyHit(damage);
+            }
+        }
+    }
+
+    void SlasheffectAngle(GameObject _slashEffect, int _effectAngle, Transform _attackTransform)
+    {
+        _slashEffect = Instantiate(_slashEffect, _attackTransform);
+        _slashEffect.transform.eulerAngles = new Vector3(0, 0, _effectAngle);
+        _slashEffect.transform.localScale = new Vector2(transform.localScale.x, transform.localScale.y);
+    }
+}
---
+++ Assets/Scripts/PlayerStateList.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStateList : MonoBehaviour
+{
+    public bool jumping = false;
+    public bool dashing = false;
+    public bool wallRunning = false;
+    public bool running = false;
+    public bool wallSliding = false;
+}
